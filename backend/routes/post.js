02b7c9cf4c61a84af04a0694b3c943b3ff354b50
anvyidol <phanleho002@gmail.com>
2022-04-08 16:29:27 +0700

    ignore BE

--- backend/routes/post.js
+++
@@ -1,8 +0,0 @@
-const route = require("express").Router();
-const userController = require("../controllers/PostControllers");
-
-route.post("/upload-avt", userController.uploadAvt);
-route.post("/new-post", userController.newPost);
-route.get("/all-post", userController.getAllPost);
-
-module.exports = route;
